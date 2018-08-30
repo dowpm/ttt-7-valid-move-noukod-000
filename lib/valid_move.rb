b7d9daf97808c3d7338e1a7e19bcc34ddf655305
Mac Dowell Prosper <macdowell@noukod.com>
2018-08-30 19:45:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,10 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(array,index)
+  if (array[index] == " " or array[index] == "" or array[index] == nil)
+    return false
+  else
+    return true
+  end
+end
