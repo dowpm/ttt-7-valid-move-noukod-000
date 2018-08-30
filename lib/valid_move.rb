d662bc81ba1af6c3e44a684aae4c4d1ebfe93da9
Mac Dowell Prosper <macdowell@noukod.com>
2018-08-30 20:03:20 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,11 @@
 # code your #valid_move? method here
 def valid_move?(array,index)
 
+  if index.between(, 8)
+    return position_taken?(array,int_index)
+  else
+    return false
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
