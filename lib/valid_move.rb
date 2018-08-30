2b7237b8a929442d1b69058ac9fbdde60ed90d45
Mac Dowell Prosper <macdowell@noukod.com>
2018-08-30 20:09:42 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(array,index)
   if index >= 0 and index <= 8
-    return position_taken?(array,int_index)
+    return position_taken?(array,index)
   else
     return false
   end
